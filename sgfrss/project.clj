2fd9f8e22a2994030894f18971c2cbe39591aec2
Usidedown <usidedown@gmail.com>
2013-12-28 18:37:05 +0200

    Advanced version

--- sgfrss/project.clj
+++ sgfrss/project.clj
@@ -1,4 +1,4 @@
-(defproject sgfrss "0.1.0-SNAPSHOT"
+(defproject sgfrss "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
