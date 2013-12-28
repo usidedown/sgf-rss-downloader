250793093da9f63984bb687138520538dfdc25fd
Usidedown <usidedown@gmail.com>
2013-12-28 16:05:47 +0200

    Fixed bug where choosing a dir won't do anything

--- sgfrss/src/sgfrss/config.clj
+++ sgfrss/src/sgfrss/config.clj
@@ -9,7 +9,11 @@
 ;Since we serialized as java dates, we need to update what we read.
 (defn read-config
   [config-file]
-  (update-in (frm-load (as-file config-file)) [:last-date] from-date))
+  (let [config-map (frm-load (as-file config-file))]
+    (update-in
+      (update-in config-map
+                 [:working-dir] file)
+      [:last-date] from-date)))
 
 (defn write-config
   [config-file config-map]
@@ -23,3 +27,12 @@
 (defmethod print-method org.joda.time.DateTime
   [^org.joda.time.DateTime d out]
   (print-method (.toDate d) out))
+
+;;These methods help us serialize java files to paths
+(defmethod print-dup java.io.File
+  [^java.io.File d out]
+  (print-dup (.getCanonicalPath d) out))
+
+(defmethod print-method java.io.File
+  [^java.io.File d out]
+  (print-method (.getCanonicalPath d) out))
--- sgfrss/src/sgfrss/core.clj
+++ sgfrss/src/sgfrss/core.clj
@@ -5,7 +5,6 @@
         clojure.stacktrace
         seesaw.core
         seesaw.chooser
-        [clojure.java.io :only [as-file]]
         [clj-time.core :only [now]]
         [clojure.tools.logging :only [info error]])
   (:import java.io.File)
@@ -17,8 +16,8 @@
          (error "log-excepions caught error" (.getMessage e#)))))
 
 (defn download-no-throw
-  [url]
-  (log-exceptions (download url)))
+  [& args]
+  (log-exceptions (apply download args)))
 
 (defmacro make-config-if-needed
   [config-file config-map]
@@ -30,11 +29,11 @@
   "I don't do a whole lot."
   [& args]
   (make-config-if-needed config-file {:last-date (now)
-                                      :working-dir (.getCanonicalPath (choose-file :selection-mode :dirs-only :remember-directory? false))})
-  (let [{date :last-date working-dir #((as-file (:working-dir %1))) :as config-map} (read-config config-file) 
+                                      :working-dir (choose-file :selection-mode :dirs-only :remember-directory? false)})
+  (let [{date :last-date working-dir :working-dir :as config-map} (read-config config-file) 
         entries (get-entries-sequence (parse-gokifu))]
     (doseq [link (map :sgf-url (get-newer-entries entries date))]
-      (download link))
+      (download-no-throw link working-dir))
     (write-config config-file 
                   (assoc config-map 
                          :last-date (:publish-date (first entries))))))
