2254153f9261968310e5858800f5abc91a50fcad
Usidedown <usidedown@gmail.com>
2013-12-28 17:29:33 +0200

    Won't download existing files

--- sgfrss/src/sgfrss/downloader.clj
+++ sgfrss/src/sgfrss/downloader.clj
@@ -1,6 +1,7 @@
 (ns sgfrss.downloader
   (:use clojure.java.io)
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client])
+  (:import java.io.File))
 
 (defn http-response-to-filename
   "Get the name of the file downloaded from the http response"
@@ -22,5 +23,7 @@
   ([src-url]
     (download src-url "."))
   ([src-url download-dir]
-  (let [out-file (download-url-to-filename src-url)]
-    (copy (input-stream src-url) (file download-dir out-file) :encoding "ASCII"))))
+  (let* [out-file-name (download-url-to-filename src-url)
+         out-file (file download-dir out-file-name)]
+    (when-not (.exists out-file)
+      (copy (input-stream src-url) out-file :encoding "ASCII")))))
